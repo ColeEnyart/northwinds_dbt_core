7be617b6192115d12c640a3471102183986b05e3
Cole Enyart <letchro@gmail.com>
2024-02-06 18:58:59 -0600

    correct fizzbuzz

--- northwinds_dbt/models/mart/join_test_contacts.sql
+++ northwinds_dbt/models/mart/join_test_contacts.sql
@@ -5,17 +5,11 @@
 -- WHERE d.company_pk = '93ff992c29e86cfd7f8705a995fd1cf5'
 
 
-{% for i, num in enumerate(range(0, 6)) %}
+{% for num in range(0, 6) %}
     {% if num % 2 == 0 %}
-        print(i, 'fizz') 
+        {% set value = 'fizz' %}
     {% else %}
-        print(i, 'buzz')
+        {% set value = 'buzz' %}
     {% endif %}
-
+    select {{ num }} as number,  '{{ value }}' as output {% if not loop.last %} union all {% endif %}
 {% endfor %}
-
--- for i, num in enumerate(range(0, 6)):
---     if num % 2 == 0:
---         print(i, 'fizz')
---     else:
---         print(i, 'buzz')
