0f32ea7bbf2a72366f2cdf5fcb2ab74ba0848b89
Cole Enyart <letchro@gmail.com>
2024-02-06 18:25:28 -0600

    add fizzbuzz

--- northwinds_dbt/models/mart/join_test_contacts.sql
+++ northwinds_dbt/models/mart/join_test_contacts.sql
@@ -1,5 +1,21 @@
-SELECT *
-FROM dev.mart_companies_dim d
-JOIN dev.mart_contacts_dim c
-ON d.company_pk = c.company_pk
-WHERE d.company_pk = '93ff992c29e86cfd7f8705a995fd1cf5'
+-- SELECT *
+-- FROM dev.mart_companies_dim d
+-- JOIN dev.mart_contacts_dim c
+-- ON d.company_pk = c.company_pk
+-- WHERE d.company_pk = '93ff992c29e86cfd7f8705a995fd1cf5'
+
+
+{% for i, num in enumerate(range(0, 6)) %}
+    {% if num % 2 == 0 %}
+        print(i, 'fizz') 
+    {% else %}
+        print(i, 'buzz')
+    {% endif %}
+
+{% endfor %}
+
+-- for i, num in enumerate(range(0, 6)):
+--     if num % 2 == 0:
+--         print(i, 'fizz')
+--     else:
+--         print(i, 'buzz')
